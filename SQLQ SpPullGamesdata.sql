0a1d9255543b522bf511554739eaf097b3e0ef8d
Abeokuta2020 <btj2014@gmail.com>
2024-08-06 20:35:27 -0400

    Add files via upload

--- SQLQ SpPullGamesdata.sql
+++ SQLQ SpPullGamesdata.sql
@@ -1,11 +1,12 @@
 Create Procedure SpPullGamesdata
 as
+--Fetching all records
 SELECT [Year]
       ,[City]
   FROM [SQLTraining].[dbo].[Games]
 
  execute SpPullGamesdata
-
+ --update table
  update Games
  set Year = 2025
  where year = 2000
